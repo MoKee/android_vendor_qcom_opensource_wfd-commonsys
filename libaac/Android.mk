LineageOS/android_hardware_qcom_media

libaac: Add test code for AAC encoder and decoder
-- unit test code for AAC encoder and decoder.

Change-Id: I6af9030d485dd4151a84f5f4870da4b2c4c45dfa

libaac/Android.mk
@@ -1,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := aacEncode.cpp
+LOCAL_SRC_FILES += aacDecode.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACenc/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACdec/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libSYS/include
+
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_STATIC_LIBRARIES := libFraunhoferAAC
+
+LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE := libaacwrapper
+
+include $(BUILD_SHARED_LIBRARY)

MoKee/android_vendor_qcom_opensource_wfd-commonsys

libaac/Android.mk
@@ -1,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := aacEncode.cpp
+LOCAL_SRC_FILES += aacDecode.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACenc/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACdec/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libSYS/include
+
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_STATIC_LIBRARIES := libFraunhoferAAC
+
+LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE := libwfdaac
+
+include $(BUILD_SHARED_LIBRARY)