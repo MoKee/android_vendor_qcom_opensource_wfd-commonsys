acdb81bdf4821849d01a3c5fc6a663d7513b1eaa
Lubin Yin <lubiny@codeaurora.org>
2018-11-28 11:52:34 -0800

    libaac: Move libaac code to commonsys-intf
    Move libaac code to commonsys-intf
    
    Change-Id: I1cf375904d6ba1670c1ffffa5cd46e3b862ce670

--- libaac/Android.mk
+++ libaac/Android.mk
@@ -16,3 +16,25 @@ LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE := libwfdaac
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := aacEncode.cpp
+LOCAL_SRC_FILES += aacDecode.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACenc/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libAACdec/include
+LOCAL_C_INCLUDES += $(TOP)/external/aac/libSYS/include
+
+LOCAL_HEADER_LIBRARIES += libutils_headers
+
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_STATIC_LIBRARIES := libFraunhoferAAC
+
+LOCAL_PROPRIETARY_MODULE := true
+
+LOCAL_32_BIT_ONLY := true
+LOCAL_MODULE := libwfdaac_proprietary
+
+include $(BUILD_SHARED_LIBRARY)
