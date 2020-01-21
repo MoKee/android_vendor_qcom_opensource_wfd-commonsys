1e18ad9befebffd3892801e7bca0e51458445807
Manu Prasad <mpras@codeaurora.org>
2020-01-21 15:30:58 +0530

    libaac: Disable WFD feature
    Change-Id: Iacac14b3e266472036b052ab2616bd021776d6b8

--- Android.mk
+++ Android.mk
@@ -1,2 +1,12 @@
 LOCAL_PATH := $(call my-dir)
+
+#Disable WFD for selected 32-bit targets
+ifeq ($(call is-board-platform,bengal),true)
+ifeq ($(TARGET_BOARD_SUFFIX),_32)
+WFD_DISABLE_PLATFORM_LIST := bengal
+endif
+endif
+
+ifneq ($(call is-board-platform-in-list,$(WFD_DISABLE_PLATFORM_LIST)),true)
 include $(call all-makefiles-under, $(LOCAL_PATH))
+endif
