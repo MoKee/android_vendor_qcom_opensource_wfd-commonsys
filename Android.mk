2fefd776cb17654b676c8a567668ed47e4d18b66
Bruno Martins <bgcngm@gmail.com>
2021-01-12 21:29:07 +0000

    wfd-commonsys: Guard against non-QSSI_SUPPORTED_PLATFORMS
    Change-Id: If1ff58d17f923fd6f0c4906620d38ca0d36cd193

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
+
 #Disable WFD for selected 32-bit targets
 ifeq ($(call is-board-platform,bengal),true)
 ifeq ($(TARGET_BOARD_SUFFIX),_32)
@@ -11,4 +13,6 @@ ifneq ($(call is-board-platform-in-list,$(WFD_DISABLE_PLATFORM_LIST)),true)
 ifneq ($(TARGET_HAS_LOW_RAM), true)
 include $(call all-makefiles-under, $(LOCAL_PATH))
 endif
+endif
+
 endif
